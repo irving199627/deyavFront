3197bb2568b97530e12f15a4a551d1d4f2e0fa09
irving199627 <irving_rosado01@hotmail.com>
2019-07-21 18:58:15 -0500

    correcciones, admin listo

--- src/app/services/cargar-imagenes.service.ts
+++ src/app/services/cargar-imagenes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { AngularFireStorage } from '@angular/fire/storage';
-import * as firebase from 'firebase';
+// import { AngularFireStorage } from '@angular/fire/storage';
+// import * as firebase from 'firebase';
 import { Observable } from 'rxjs';
 import { finalize, map } from 'rxjs/operators';
 @Injectable({
@@ -11,51 +11,51 @@ export class CargarImagenesService {
   progreso;
   uploadPercent: Observable<number>;
   downloadURL: Observable<string>;
-  constructor( private storage: AngularFireStorage ) { }
+  constructor( ) { }
 
-  cargarImagenesFirebase( imagenes: any, tipo ) {
-    // console.log(imagenes);
-    const fecha = new Date();
-    const url = `${ this.CARPETA_IMAGENES }/${ tipo }/${fecha.getMilliseconds()}`;
-    const storageRef = this.storage.ref(url);
-    const task = storageRef.putString(imagenes, 'base64', { contentType: 'image/jpeg'});
-    this.uploadPercent = task.percentageChanges();
-    task.snapshotChanges().subscribe(() => {
-      this.uploadPercent.subscribe( a => {
-        console.log(a);
-      });
-    });
-    task.snapshotChanges().pipe(
-      finalize(() => {
-        this.downloadURL = storageRef.getDownloadURL();
-        storageRef.getDownloadURL().subscribe(res => {
-            console.log(res);
-        });
-      })
-   )
-  .subscribe();
+  // cargarImagenesFirebase( imagenes: any, tipo ) {
+  //   // console.log(imagenes);
+  //   const fecha = new Date();
+  //   const url = `${ this.CARPETA_IMAGENES }/${ tipo }/${fecha.getMilliseconds()}`;
+  //   const storageRef = this.storage.ref(url);
+  //   const task = storageRef.putString(imagenes, 'base64', { contentType: 'image/jpeg'});
+  //   this.uploadPercent = task.percentageChanges();
+  //   task.snapshotChanges().subscribe(() => {
+  //     this.uploadPercent.subscribe( a => {
+  //       console.log(a);
+  //     });
+  //   });
+  //   task.snapshotChanges().pipe(
+  //     finalize(() => {
+  //       this.downloadURL = storageRef.getDownloadURL();
+  //       storageRef.getDownloadURL().subscribe(res => {
+  //           console.log(res);
+  //       });
+  //     })
+  //  )
+  // .subscribe();
 
-    // const uploadTask: firebase.storage.UploadTask =
-    //         storageRef.child(url)
-    //                   .put( imagenes );
+  //   // const uploadTask: firebase.storage.UploadTask =
+  //   //         storageRef.child(url)
+  //   //                   .put( imagenes );
 
-    // uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-    //   ( snapshot: firebase.storage.UploadTaskSnapshot )  => {
-    //     this.progreso = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100;
-    //     console.log(this.progreso);
-    //   },
-    //   ( error ) => console.error('Error al subir', error),
-    //   () => {
-    //           console.log('imagen cargada correctamente');
-    //           this.url = uploadTask.snapshot.downloadURL;
-    //           this.guardarImagen(this.url);
-    //   });
+  //   // uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+  //   //   ( snapshot: firebase.storage.UploadTaskSnapshot )  => {
+  //   //     this.progreso = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100;
+  //   //     console.log(this.progreso);
+  //   //   },
+  //   //   ( error ) => console.error('Error al subir', error),
+  //   //   () => {
+  //   //           console.log('imagen cargada correctamente');
+  //   //           this.url = uploadTask.snapshot.downloadURL;
+  //   //           this.guardarImagen(this.url);
+  //   //   });
 
-    // console.log(storageRef);
-    // const uploadTask: firebase.storage.UploadTask = storageRef.;
-    // uploadTask.on()
-  }
-  guardarImagen( url ) {
-    console.log(url);
-  }
+  //   // console.log(storageRef);
+  //   // const uploadTask: firebase.storage.UploadTask = storageRef.;
+  //   // uploadTask.on()
+  // }
+  // guardarImagen( url ) {
+  //   console.log(url);
+  // }
 }
