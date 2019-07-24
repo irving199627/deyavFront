999337bc33a56dc1ea8cf5452fbf2b7707d5adab
irving199627 <irving_rosado01@hotmail.com>
2019-07-24 13:53:33 -0500

    CRUD cursos listo

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,3 @@
 // export const URL_SERVICIOS = 'http://31.220.59.7/api/v1';
-// export const URL_SERVICIOS = 'http://psicologadeya.com/api/v1';
-export const URL_SERVICIOS = 'http://localhost:3000/api/v1';
+export const URL_SERVICIOS = 'http://psicologadeya.com/api/v1';
+// export const URL_SERVICIOS = 'http://localhost:3000/api/v1';
